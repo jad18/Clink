17a4f11b990df32d2ceb873bfcd738854fc2ac13
Jackson DeWitt <jacksondewitt18@new-host-13.home>
2020-05-07 15:08:25 -0400

    More changes to the profile-changing forms

--- clink/src/components/App.js
+++ clink/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom';
+import {Route, Switch, Link, Redirect} from 'react-router-dom';
 
 //Page imports
 import HomePage from './home_page.js';
@@ -12,31 +12,36 @@ function App() {
   return (
     <>
     <div className="App">
-      <h1 size='40'>Welcome to Clink!</h1>
-      <h4>Find friends anonymously</h4>
-      <Router>
-        <hr/>
-        <div class="button-group">
-          <Link to='/'>
-            <button class="link-button1">Home</button>
-          </Link>
-          <Link to='/change_profile'>
-            <button class="link-button1">Next</button>
-          </Link>
-          <Link to='/sports'>
-            <button class="link-button1">Sports</button>
-          </Link>
-          <button class="link-button1">When</button>
+      <h1 id='top-header'>Welcome to Clink!</h1>
+      <h2>Find friends anonymously</h2>
+
+      <hr/>
+      <div class="button-group">
+
+        <Link to='/'>
+          <button class="link-button1">Home</button>
+        </Link>
+
+        <Link to='/change_profile/sports'>
+          <button class="link-button1">Profile</button>
+        </Link>
+
+        <button class="link-button1">Messages</button>
+
+        <Link to='/change_profile'>
+          <button id="change-profile-button">Change Profile</button>
+        </Link>
+
         </div>
         <hr/>
+
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/change_profile" component={NextPage}/>
-          <Route exact path="/sports" component={SportsForm}/>
+          <Route exact path="/change_profile/sports" component={SportsForm}/>
           <Route exact path="/404_error" component={ErrorPage}/>
           <Redirect to="/404_error"/>
         </Switch>
-      </Router>
       
     </div>
     </>
@@ -44,6 +49,3 @@ function App() {
 }
 
 export default App;
-
-
-/* browserHistory.push('/next-page') */
--- clink/src/components/sports_form.js
+++ clink/src/components/sports_form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const SportsForm = () => {
     return (
@@ -28,12 +29,85 @@ const SportsForm = () => {
             </div>
             </div>
   
+            <div class='form-row'>
+            <div class='form-column'>
             <p><input type = "checkbox"
-                id = "Baseball"
-                value = "Baseball" />
-            <label for = "Baseball">Baseball</label></p>
+                id = "Football"
+                value = "Football" />
+            <label for = "Football">Football</label></p>
+            </div>
+
+            <div class='form-column'>
+            <p><input type = "checkbox"
+                id = "Soccer"
+                value = "Soccer" />
+            <label for = "Soccer">Soccer</label></p>
+            </div>
+            </div>
+
+            <div class='form-row'>
+            <div class='form-column'>
+            <p><input type = "checkbox"
+                id = "Football"
+                value = "Football" />
+            <label for = "Football">Football</label></p>
+            </div>
+
+            <div class='form-column'>
+            <p><input type = "checkbox"
+                id = "Soccer"
+                value = "Soccer" />
+            <label for = "Soccer">Soccer</label></p>
+            </div>
+            </div>
+
+            <div class='form-row'>
+            <div class='form-column'>
+            <p><input type = "checkbox"
+                id = "Football"
+                value = "Football" />
+            <label for = "Football">Football</label></p>
+            </div>
+
+            <div class='form-column'>
+            <p><input type = "checkbox"
+                id = "Soccer"
+                value = "Soccer" />
+            <label for = "Soccer">Soccer</label></p>
+            </div>
+            </div>
+
+            <div class='form-row'>
+            <div class='form-column'>
+            <p><input type = "checkbox"
+                id = "Football"
+                value = "Football" />
+            <label for = "Football">Football</label></p>
+            </div>
+
+            <div class='form-column'>
+            <p><input type = "checkbox"
+                id = "Soccer"
+                value = "Soccer" />
+            <label for = "Soccer">Soccer</label></p>
+            </div>
+            </div>
   
         </form>
+
+        <p></p>
+        <Link to='/change_profile/sports'>
+                <button class="link-button2">Submit Changes</button>
+            </Link>
+        <p></p>
+        <p>
+            <Link to='/change_profile'>
+                <button class="link-button2">Back to Profile Change</button>
+            </Link>
+            <Link to='/'>
+                <button class="link-button2">Next Profile Section</button>
+            </Link>
+        </p>
         </>
     );
 }

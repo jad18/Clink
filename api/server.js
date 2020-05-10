1bf51ef1315e585118c5a8acb888462cfcd195ba
Jeremy Tsai <jeremyt1114@g.ucla.edu>
2020-05-11 04:08:09 +0800

    Required some useful things. Check comments for intended use. Might turn out not useful/inaccurate.

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const ​express = ​require​(​"express"​);
+const session = require('express-session'); // so user can stay logged in
 const ​app = express();
-app.​get​(​"/"​, (req, res) =>
+
+app.use(express.urlencoded({extended: false})); // so the json won't be sent through the URL
+
+app.​get​(​"/"​, (req, res) =>{
   ​// TODO
-​  res.​send​(​"Hello, world!"​); });
-  app.​listen​(​3000​, () => { ​console​.​log​(​"Listening on port 3000!"​);
+​  res.​send​(​"Hello, world!"​); 
+});
+
+app.​listen​(​3000​, () => { ​
+  console​.​log​(​"Listening on port 3000!"​);
 });

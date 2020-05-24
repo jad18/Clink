b82b505534f96081b66a92790e808b23f289b90c
Jackson DeWitt <jacksondewitt18@jacksons-mbp.myfiosgateway.com>
2020-05-24 16:34:18 -0400

    Added links to forms in the profile home page

--- clink/src/components/profile_change_home.js
+++ clink/src/components/profile_change_home.js
@@ -19,18 +19,34 @@ const ChangeProfilePage = () => {
                         </Link>
                     </td>
                     <td className='table-column'>
-                    <Link to='/change_profile/movies'>
+                        <Link to='/change_profile/movies'>
                             <button className='link-button3'>TV/Movies</button>
                         </Link>
                     </td>
                 </tr>
                 <tr className='table-row'>
-                    <td className='table-column'>This is number 3.</td>
-                    <td className='table-column'>This is number 4.</td>
+                    <td className='table-column'>
+                        <Link to='/change_profile/outdoor_activities'>
+                            <button className='link-button3'>Outdoor Activities</button>
+                        </Link>
+                    </td>
+                    <td className='table-column'>
+                        <Link to='/change_profile/indoor_activities'>
+                            <button className='link-button3'>Indoor Activities</button>
+                        </Link>
+                    </td>
                 </tr>
                 <tr className='table-row'>
-                    <td className='table-column'>This is number 5.</td>
-                    <td className='table-column'>This is number 6.</td>
+                    <td className='table-column'>
+                        <Link to='/change_profile/cuisines'>
+                            <button className='link-button3'>Cuisines</button>
+                        </Link>
+                    </td>
+                    <td className='table-column'>
+                        <Link to='/change_profile/arts_and_media'>
+                            <button className='link-button3'>Arts and Media</button>
+                        </Link>
+                    </td>
                 </tr>
                 </tbody>
             </table>

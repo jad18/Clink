0f9552d005664a730fe5cd4d5b2e3e97ac8f30a9
Rohan Battula <rohanbattula@gmail.com>
2020-05-29 18:23:17 -0700

    implemented mongoose, fixing passport too

---
+++ api/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+// Create Schema
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { strict: false }
+);
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = User = mongoose.model("User", UserSchema);
--- api/passport-config.js
+++ api/passport-config.js
@@ -1,3 +1,29 @@
+
+module.exports = function(passport){
+
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	  });
+	  
+	passport.deserializeUser(function(id, done) {
+		User.findById(id, function (err, user) {
+		  done(err, user);
+		});
+	  });
+
+	passport.use(new LocalStrategy(
+		function(username, password, done) {
+		  User.findOne({ username: username }, function (err, user) {
+			if (err) { return done(err); }
+			if (!user) { return done(null, false); }
+			if (!user.verifyPassword(password)) { return done(null, false); }
+			return done(null, user);
+		  });
+		}
+	  ));
+	
+}
+/*
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 function initialize(passport, getUserByUsername, getUserById)
@@ -35,3 +61,4 @@ function initialize(passport, getUserByUsername, getUserById)
 
 
 module.exports = initialize;
+*/

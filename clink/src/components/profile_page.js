65010f4b7710964962a2f0a81558ee8ab2840f0b
Jackson DeWitt <jacksondewitt18@jacksons-mbp.myfiosgateway.com>
2020-06-01 00:41:15 -0400

    Standardizing the tags used and generalizing the vertical spacing in the profile page to allow for better use across different screen sizes and machines

--- clink/src/components/profile_page.js
+++ clink/src/components/profile_page.js
@@ -135,7 +135,69 @@ export default class Profile extends React.Component {
   render() {
     return (
       <div className="App">
-        <div id="stripes" />
+
+        <h1>Your Profile</h1>
+
+        <div id="outer-box">
+          <div id="stripes" />
+                    
+          <div id="Pers">
+              <u><h4 className="section-header">Personality</h4></u>
+              {extractPersonalityItems(JSON.parse(sessionStorage.getItem("profile_personality")))}
+          </div>
+                
+          <div id="Pinfo">
+            <u><h4 className="section-header">Personal Information</h4></u>
+            {extractPersonalInfoItems(JSON.parse(sessionStorage.getItem("profile_personalInfo")))}
+          </div>
+                
+          <div id="bio">
+            <u><h4 className="section-header">About Me</h4></u>
+            {sessionStorage.getItem("bio")}
+          </div>
+          <div id="hobbies">
+            <u><h4 className="section-header">Interests</h4></u>
+            <ul id="interest-list">
+              <li>
+                <strong>Sports:</strong>
+                {extractListItems(JSON.parse(sessionStorage.getItem("profile_sports")))}
+              </li>
+              <li>
+                <strong>Movies:</strong>
+                {extractListItems(JSON.parse(sessionStorage.getItem("profile_movies")))}
+              </li>
+              <li>
+                <strong>Indoor Activities:</strong>
+                {extractListItems(JSON.parse(sessionStorage.getItem("profile_indoor")))}
+               </li>
+              <li>
+                <strong>Outdoor Activities:</strong>
+                {extractListItems(JSON.parse(sessionStorage.getItem("profile_outdoor")))}
+              </li>
+              <li>
+                <strong>Types of Food:</strong>
+                {extractListItems(JSON.parse(sessionStorage.getItem("profile_cuisines")))}
+              </li>
+              <li>
+                <strong>Arts, Theater, and Media:</strong>
+                {extractListItems(JSON.parse(sessionStorage.getItem("profile_arts")))}
+              </li>
+            </ul>
+          </div>
+                    
+          <div id="card">
+            <h2>{sessionStorage.getItem("username")}</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+/*
+ <div id="stripes" />
 
         <div id="card">
           <h2>{sessionStorage.getItem("username")}'s Profile</h2>
@@ -187,8 +249,160 @@ export default class Profile extends React.Component {
               </span>
             </div>
           </ul>
-        </div>
-      </div>
-    );
-  }
+        </div>*/
+
+
+/*
+.App {
+  font-family: sans-serif;
+  text-align: center;
 }
+#stripes {
+  background: #999955;
+  background-image: linear-gradient(
+    #dab046 2%,
+    rgb(212, 186, 182) 20%,
+    rgb(247, 109, 93) 40%,
+    rgb(133, 199, 27) 30%,
+    rgb(236, 137, 142) 60%,
+    rgb(212, 113, 138) 60%,
+    rgb(116, 93, 99) 80%,
+    #a38f98 80%
+  );
+  margin: 0 auto;
+  margin-top: 235px;
+  width: 100%;
+  height: 150px;
+  box-shadow: -30px 0 35px -25px black, 20px 0 45px -25px black;
+}
+#stripes:before {
+  content: "";
+  position: absolute;
+  background: rgb(241, 224, 180);
+  border-radius: 15px;
+  z-index: -1;
+  margin-top: 257px;
+  width: 950px;
+  height: 520px;
+  padding: 25px;
+  padding-top: 0;
+  padding-bottom: 0;
+  left: 48%;
+  top: 66px;
+  margin-left: -444px;
+  box-shadow: #333;
+}
+
+#card {
+  position: absolute;
+  border-radius: 10px;
+  margin-top: 300px;
+  width: 850px;
+  height: 435px;
+  padding: 25px;
+  padding-top: 0;
+  padding-bottom: 0;
+  left: 48%;
+  top: 67.5px;
+  margin-left: -392px;
+  background: #e9e2d0;
+  box-shadow: -30px 0 35px -25px black, 20px 0 45px -25px black;
+  z-index: 1;
+}
+
+#card h2 {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+  "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  sans-serif;
+  color: #333;
+  margin: 0 auto;
+  padding: 10px;
+  font-size: 15pt;
+}
+
+#card p {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  color: rgb(39, 38, 38);
+  font-size: 16px;
+}
+
+.section-header
+{
+  margin: 0px;
+  text-align: center;
+  margin-bottom: 8px;
+}
+
+
+#card span {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  line-height: 140%;
+  font-size: 14px;
+}
+#bio {
+  background-color: rgb(226, 223, 215);
+  height: 340px;
+  position: absolute;
+  padding: 8px;
+  margin-top: -10px;
+  border-radius: 10px;
+  width: 200px;
+  margin-left: -170px;
+  box-shadow: -30px 0 35px -25px rgb(128, 118, 118),
+    20px 0 45px -25px rgb(128, 118, 118);
+  color: rgb(0, 0, 0);
+  text-align: left;
+  overflow: scroll;
+}
+
+
+#Pers {
+  background-color: rgb(226, 223, 215);
+  height: 150px;
+  position: absolute;
+  padding: 8px;
+  margin-top: -10px;
+  border-radius: 10px;
+  width: 200px;
+  margin-left: -430px;
+  box-shadow: -30px 0 35px -25px rgb(128, 118, 118),
+    20px 0 45px -25px rgb(128, 118, 118);
+  color: rgb(0, 0, 0);
+  text-align: left;
+}
+#Pinfo {
+  background-color: rgb(226, 223, 215);
+  height: 150px;
+  position: absolute;
+  padding: 8px;
+  margin-top: 180px;
+  border-radius: 10px;
+  width: 200px;
+  margin-left: -430px;
+  box-shadow: -30px 0 35px -25px rgb(128, 118, 118),
+    20px 0 45px -25px rgb(128, 118, 118);
+  color: rgb(0, 0, 0);
+  text-align: left;
+}
+#hobbies {
+  background-color: rgb(226, 223, 215);
+  height: 340px;
+  position: absolute;
+  padding: 8px;
+  margin-top: -10px;
+  border-radius: 20px;
+  width: 300px;
+  margin-left: 490px;
+  box-shadow: -30px 0 35px -25px rgb(128, 118, 118),
+    20px 0 45px -25px rgb(128, 118, 118);
+  color: rgb(0, 0, 0);
+  text-align: left;
+}
+#span {
+  margin-bottom: 10px;
+}
+*/
